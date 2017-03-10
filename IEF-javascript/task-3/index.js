a6e711649e8333ce2cef0ec4afb627704948997c
bill <liushuangbill@outlook.com>
2017-03-10 20:43:20 +0800

    task-3

--- IEF-javascript/task-3/index.js
+++ IEF-javascript/task-3/index.js
@@ -1,21 +1,16 @@
 var button=document.getElementById("sort-btn");
 button.onclick=function(){
-    var source=document.getElementById("source");
-    var arr=source.getElementsByTagName("li");
+    var arr=document.querySelectorAll("#source>li");
     var ul=document.getElementById("resort");
-    var arr1=[];
-    for(var i=0;i<arr.length;i++){
-          arr1.push(arr[i].innerText);
-    }
-    arr1.forEach((x,i)=>{
-        arr1[i]=x.split("：");
-    });
-    arr1.sort((x1,x2)=>{
-        if(x1[1]<x2[1]) return 1;
-        else return 0;
+    [].slice.call(arr).map(x=>{
+        return x.innerText.split("：");
+    }).sort((x1,x2)=>{
+        return x2[1]-x1[1];
     }).forEach(x=>{
-        var li=document.createElement("li");
-        li.innerHTML=x[0]+":"+x[1];
-        ul.appendChild(li);
-    })
+        if(ul.childNodes.length<=6){
+            var li=document.createElement("li");
+            li.innerHTML=x[0]+":"+x[1];
+            ul.appendChild(li);
+        }
+    });
 }
